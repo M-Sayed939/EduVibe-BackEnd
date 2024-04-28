0a263006b94c43cb4b6d8a0d03d33be5a5727960
M-Sayed939 <104098022+M-Sayed939@users.noreply.github.com>
2024-04-28 21:09:00 +0300

    Initialize

---
+++ src/main/java/com/example/demo/Initializing/DatabaseInitializer.java
@@ -0,0 +1,26 @@
+package com.example.demo.Initializing;
+
+import com.example.demo.StudyStrategy.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import javax.annotation.PostConstruct;
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class DatabaseInitializer {
+
+    @Autowired
+    private StudyStrategyService studyStrategyService;
+
+    @PostConstruct
+    public void initDatabase() {
+        List<StudyStrategy> studyStrategies = Arrays.asList(
+                new StudyStrategy("Study Strategy 1", "Description 1"),
+                new StudyStrategy("Study Strategy 2", "Description 2")
+        );
+        for (StudyStrategy studyStrategy : studyStrategies) {
+            studyStrategyService.addStudyStrategy(studyStrategy);
+        }
+    }
+}
--- src/main/java/com/example/demo/StudyStrategy/StudyStrategy.java
+++ src/main/java/com/example/demo/StudyStrategy/StudyStrategy.java
@@ -2,17 +2,23 @@ package com.example.demo.StudyStrategy;
 
 import com.example.demo.Course.Course;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
 @Data
+@NoArgsConstructor
 public class StudyStrategy {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String description;
+    public StudyStrategy(String name, String description) {
+    this.name = name;
+    this.description = description;
+}
 
     @ManyToOne
     @JoinColumn(name = "course_id")
